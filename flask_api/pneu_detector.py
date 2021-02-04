92eb3c11c140dfa9e45b213a923212fc53721791
sanou <desiresandes@gmail.com>
2021-02-04 14:27:28 +0100

    Tests on pneumonia_detestor updates

--- flask_api/pneu_detector.py
+++ flask_api/pneu_detector.py
@@ -7,6 +7,7 @@ class PneumoniaDetector(object):
     def __init__(self, model_path):
         self.prd_model = load_model(model_path)
 
+
     @staticmethod
     def __process_data(img_path):
         """
@@ -21,24 +22,28 @@ class PneumoniaDetector(object):
         img = np.array(img)
         return img
 
-    def __predict(self, image_file):
-        pred_probas = self.prd_model.predict(image_file)
+    def predict(self, image_file):
+        img_proc = self.__class__.__process_data(image_file)
+        pred_probas = self.prd_model.predict(img_proc)
         pred_index = np.argmax(pred_probas, axis=1)
-        pred_confidence = pred_probas[pred_index]
-        return pred_index, round(pred_confidence, 2)
+        pred_confidence = pred_probas[0][pred_index]
+        return pred_index, np.round(pred_confidence, 2)
 
     def check_pneumonia(self, image_file, filename=""):
-        error_message = "Failed to predict the class "
+        error_message = "Failed to predict the class:  "
         try:
-            img_proc = self.__class__.__process_data(image_file)
-            has_pneumonia, pred_confidence = self.__predict(img_proc)
+            has_pneumonia, pred_confidence = self.predict(image_file)
+            print("has_pneumonia:",has_pneumonia, "pred_confidence", pred_confidence )
             if has_pneumonia:
                 prediction = "Pneumonia detected"
             else:
                 prediction = "Normal Chest"
             prediction_dict = {'pred': prediction, 'proba': pred_confidence}
-            filename = filename + "pred= " + prediction + ";confidence= " + pred_confidence
+            print("prediction_dict: ", prediction_dict)
+            print("filename:", filename)
+            filename = filename + "pred= " + str(prediction) + ";confidence= " + str(pred_confidence)
+            print("filename:", filename)
         except Exception as ex:
             prediction_dict = {'pred': error_message + str(ex), 'proba': "None"}
-            filename = filename.split(".")[-2] +" "+ error_message
+            filename = filename.split(".")[0] +" "+ error_message
         return prediction_dict, filename
--- tests/pneu_detector_test.py
+++ tests/pneu_detector_test.py
@@ -1,25 +1,41 @@
+import pytest
 import unittest
-from flask_api.pneu_detector import  *
+import pneu_detector as pneu
 
-pneumonia_model_path = '../ml_model/finalModel/pneu_detect_cnn_model.h5'
+parent_dir = "C:\\Users\\salim\\Desktop\\VALDOM\\Transverse\\project\\"
+pneumonia_model_path = parent_dir+ 'ml_model/finalModel/pneu_detect_cnn_model.h5'
 image_path= "../dataprep/chest_xray/test/"
-model= PneumoniaDetector(pneumonia_model_path)
+Model= pneu.PneumoniaDetector(pneumonia_model_path)
 
-class PredictionsModelTestCase(unittest.TestCase):
-    def test_data_process(self):
-        pass
+class TestModelPredictions(unittest.TestCase):
+    def setup_class(self):
+        self.normal_image_file = image_path+"NORMAL/IM-0001-0001.jpeg"
+        self.infected_image_file = image_path+"PNEUMONIA/person1_virus_6.jpeg"
 
+    def test_predict_normal(self):
+        has_pneumonia, pred_confidence = Model.predict(self.normal_image_file)
+        self.assertEqual(has_pneumonia, False)
+        self.assertGreater(pred_confidence, 0.5)
 
-    def test_check_pneumonia(self):
-        prediction_dict, filename= model.check_pneumonia(image_path+"IM-0001-0001.jpeg")
-        expected_filename= "pred= " + prediction_dict["pred"] + ";confidence= " + prediction_dict["proba"]
+    def test_predict_pneumonia(self):
+        has_pneumonia, pred_confidence = Model.predict(self.infected_image_file)
+        self.assertEqual(has_pneumonia, True)
+        self.assertGreater(pred_confidence, 0.5)
+
+    def test_check_pneumonia_normal(self):
+        prediction_dict, filename= Model.check_pneumonia(self.normal_image_file)
+        expected_filename= "pred= " + str(prediction_dict["pred"]) + ";confidence= " + str(prediction_dict["proba"])
         self.assertEqual(prediction_dict["pred"], "Normal Chest")
         self.assertEqual(prediction_dict["proba"]>0.5, True)
         self.assertEqual(filename,expected_filename)
 
-    def test_normal_chest(self):
-        self.assertEqual(True, False)
+    def test_check_pneumonia_infected(self):
+        prediction_dict, filename = Model.check_pneumonia(self.infected_image_file)
+        expected_filename = "pred= " + str(prediction_dict["pred"]) + ";confidence= " + str(prediction_dict["proba"])
+        self.assertEqual(prediction_dict["pred"], "Pneumonia detected")
+        self.assertEqual(prediction_dict["proba"] > 0.5, True)
+        self.assertEqual(filename, expected_filename)
 
 
-if __name__ == '__main__':
-    unittest.main()
+#if __name__ == '__main__':
+ #   unittest.main()
